849916857ac9e40cd7c13fb4a1a6559fce049313
sigmotoa <sigmotoa@misena.edu.co>
2017-08-29 16:05:14 -0500

    Captura nombre y concatena en la salida

--- app/src/main/java/com/example/sigmotoa/tarea1/Actividad1.java
+++ app/src/main/java/com/example/sigmotoa/tarea1/Actividad1.java
@@ -2,6 +2,7 @@ package com.example.sigmotoa.tarea1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -31,7 +32,12 @@ public class Actividad1 extends AppCompatActivity {
         aceptar=(Button)findViewById(R.id.button);
         textosalida=(TextView)findViewById(R.id.salida);
 
-
+        aceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textosalida.setText("Hola "+nombre.getText()+" "+apellido.getText().toString());
+            }
+        });
 
 
     }
